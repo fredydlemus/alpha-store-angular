996ea49d645201811650b1f1659abda692ea2960
fredydlemus <fredyflemus@gmail.com>
2022-03-14 21:45:28 -0600

    implement proxy

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Product, CreateProductDTO, UpdateProductDTO } from '../models/product.model';
+import { retry} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  private apiUrl = 'https://young-sands-07814.herokuapp.com/api/products';
+  private apiUrl = '/api/products';
 
   constructor(private http: HttpClient) { }
 
@@ -19,7 +20,10 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', offset);
     }
-    return this.http.get<Product[]>(this.apiUrl, {params});
+    return this.http.get<Product[]>(this.apiUrl, {params})
+    .pipe(
+      retry(3)
+    );
   }
 
 
