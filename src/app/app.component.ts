968411581aa7099e74c0b85dce6db53163777459
fredydlemus <fredyflemus@gmail.com>
2022-03-21 21:31:49 -0600

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
 import { UsersService } from './services/users.service';
+import { FilesService } from './services/files.service';
+
+
 
 @Component({
   selector: 'app-root',
@@ -10,10 +13,12 @@ import { UsersService } from './services/users.service';
 export class AppComponent {
   constructor(
     private authService: AuthService,
-    private usersService: UsersService
+    private usersService: UsersService,
+    private filesService: FilesService
   ) { }
 
   token = '';
+  imgRta = '';
 
   createUser(){
     this.usersService.create({
@@ -39,5 +44,25 @@ export class AppComponent {
     .subscribe(profile =>{
       console.log(profile);
     })
+  }
+
+  downloadPdf(){
+    this.filesService.getFile('my.pdf', 'https://young-sands-07814.herokuapp.com/api/files/dummy.pdf', 'application/pdf')
+    .subscribe()
+  }
+
+  onUpload(event: Event){
+    console.log('onUpload');
+    const element = event.target as HTMLInputElement;
+    const file = element.files?.item(0);
+    console.log(file)
+    if(file){
+      this.filesService.uploadFile(file)
+    .subscribe(rta =>{
+      console.log(rta);
+      this.imgRta = rta.location;
+    })
+    }
+
   }
 }
